4670b4a5c80e4374cefebd089a5a053fe2444ed5
VenansiusRendy <lierendysetiawan3232@gmail.com>
2021-06-14 19:45:42 +0800

    Commit 1

--- ld-server-music/app.js
+++ ld-server-music/app.js
@@ -1,15 +1,13 @@
-const express = require('express');
+const express = require("express");
 
-const app = express;
+const app = express();
 
-const routes = require('./routes/');
-const errorHandler = require('./middlewares/errorHandler');
+const routes = require("./routes");
+const errorHandler = require("./middlewares/errorHandler");
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-app.use(errorHandler());
-app.use('/', routes);
-
-
+app.use(errorHandler);
+app.use("/", routes);
 
 module.exports = app;
--- ld-server-music/controllers/songController.js
+++ ld-server-music/controllers/songController.js
@@ -1,40 +1,43 @@
-const { Song } = require('../models')
+const { Song } = require("../models");
+const createError = require("http-errors");
 
 class SongController {
-  static create(req, res, next) {
-    const { title, artist, lyric } = req.body
-    Song.create({
-      title,
-      artist,
-      lyric,
-      UserId: req.loggedInUser.id
-    })
-      .then(song => {
-        res.status(201).json({
-          id: song.id,
-          title: song.title,
-          artist: song.artist,
-          lyric: song.lyric
-        })
-      })
-      .catch(next)
-  }
+	static create(req, res, next) {
+		const { title, artist, lyric } = req.body;
+		Song.create({
+			title,
+			artist,
+			lyric,
+			UserId: req.loggedInUser.id,
+		})
+			.then((song) => {
+				res.status(201).json({
+					id: song.id,
+					title: song.title,
+					artist: song.artist,
+					lyric: song.lyric,
+				});
+			})
+			.catch((err) => {
+				next(err);
+			});
+	}
 
-  static delete(req, res, next) {
-    Song.destroy({
-      id: req.params.id
-    })
-      .then(data => {
-        if(data === 1) {
-          res.status(200).json({
-            message: 'Success delete a Song'
-          })
-        } else {
-          throw createError(500, "Internal server error")
-        }
-      })
-      .catch(next)
-  }
+	static delete(req, res, next) {
+		Song.destroy({
+			id: req.params.id,
+		})
+			.then((data) => {
+				if (data === 1) {
+					res.status(200).json({
+						message: "Success delete a Song",
+					});
+				} else {
+					throw createError(500, "Internal server error");
+				}
+			})
+			.catch((err) => next(err));
+	}
 }
 
-module.exports = SongController
+module.exports = SongController;
